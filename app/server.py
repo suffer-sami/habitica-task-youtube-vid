a9989843ce1b7cfab971494b93ebe76a80090d86
suffer-sami <sami_email@protonmail.com>
2024-08-11 13:07:43 +0500

    remove basic logging config level

--- app/server.py
+++ app/server.py
@@ -6,7 +6,6 @@ from .webhook import webhook_bp
 def create_app():
     app = Flask(__name__)
 
-    logging.basicConfig(level=logging.INFO)
     app.logger.setLevel(logging.INFO)
 
     app.register_blueprint(webhook_bp)
